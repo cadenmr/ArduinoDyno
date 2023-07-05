5933bec1d341df2d6506b60c138203d7a5e0c3b1
cadenmr <37202505+cadenmr@users.noreply.github.com>
2023-07-04 21:29:39 -0700

    implement incoming data parsers

--- ArduinoDyno.ino
+++ ArduinoDyno.ino
@@ -196,7 +196,7 @@ void parseIncomingSerial() {
 // just read and drop specified amount of data
 void shredSerialData(unsigned int count) {
 
-  for (int i = 0; i > count; i++) {
+  for (int i = 0; i < count; i++) {
     Serial.read();
   }
 
@@ -211,19 +211,79 @@ boolean serialDataToBoolean() {
 
 }
 
+// 8 bit - use last byte of inc. data
 byte serialDataToByte() {
 
+  shredSerialData(4);   // shred all but last byte of data
+  byte returnData = Serial.read();
+  return returnData;
+
+
 }
 
+// 16 bit - use last 2 bytes of inc. data
 int serialDataToSignedInt() {
 
+  int returnData;
+
+  shredSerialData(3);   // shred all but last 2 bytes of data
+
+  byte upper = Serial.read();
+  byte lower = Serial.read();
+
+  uint8_t * returnDataLower = (uint8_t *)&returnData;
+  uint8_t * returnDataUpper = (uint8_t *)&returnData + 1;
+
+  *returnDataUpper = upper;
+  *returnDataLower = lower;
+
+  return returnData;
+
 }
 
+// 16 bit - use last 2 bytes of inc. data
 unsigned int serialDataToUnsignedInt() {
 
+  unsigned int returnData;
+
+  shredSerialData(3);
+
+  byte upper = Serial.read();
+  byte lower = Serial.read();
+
+  uint8_t * returnDataLower = (uint8_t *)&returnData;
+  uint8_t * returnDataUpper = (uint8_t *)&returnData + 1;
+
+  *returnDataUpper = upper;
+  *returnDataLower = lower;
+
+  return returnData;
+
 }
 
+// 32 bit - use last 4 bytes of inc. data
 double serialDataToDouble() {
+
+  double returnData;
+
+  shredSerialData(1);
+
+  byte incD3 = Serial.read();
+  byte incD2 = Serial.read();
+  byte incD1 = Serial.read();
+  byte incD0 = Serial.read();
+
+  uint8_t * d0 = (uint8_t *)&returnData;
+  uint8_t * d1 = (uint8_t *)&returnData + 1;
+  uint8_t * d2 = (uint8_t *)&returnData + 2;
+  uint8_t * d3 = (uint8_t *)&returnData + 3;
+
+  *d3 = incD3;
+  *d2 = incD2;
+  *d1 = incD1;
+  *d0 = incD0;
+
+  return returnData;
 
 }
 
