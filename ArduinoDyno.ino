676176af560c8d2f7de19ca11f543d750c21a624
cadenmr <37202505+cadenmr@users.noreply.github.com>
2023-07-08 23:44:41 -0700

    add telem and bugfix w/ if/else (ik this is bad practice)

--- ArduinoDyno.ino
+++ ArduinoDyno.ino
@@ -42,6 +42,7 @@ unsigned int            loadCellFactor                    = 0;
 // Shaft Speed Control
 unsigned int            shaftRpmCurrent                   = 0;
 unsigned int            shaftRpmCurrentRounded            = 0;
+byte * rpmPtr                                             = (byte *) &shaftRpmCurrentRounded;
 unsigned int            shaftRpmDesired                   = shaftRpmMaximum;
 volatile unsigned long  shaftHallMicrosCurrent            = 0;
 volatile unsigned long  shaftHallMicrosLast               = 0;
@@ -50,18 +51,19 @@ unsigned long           shaftRpmTimeDelta                 = 0;
 // unsigned long        shaftRpmReadings;                 // TODO: Implement this
 
 // Inlet Valve Control
-byte                    inletDutyCurrent                  = 0;
+// byte                    inletDutyCurrent                  = 0;
 byte                    inletDutyDesired                  = inletMinDuty;
 bool                    inletOverrideActive               = false;
 
 // Outlet Valve Control
-byte                    outletDutyCurrent                 = 0;
+// byte                    outletDutyCurrent                 = 0;
 byte                    outletDutyDesired                 = outletMinDuty;
 bool                    outletOverrideActive              = false;
-byte                    outletTemperatureCurrent          = 0;
+byte                    outletTemperatureCurrent          = 255;
 
 // Load Measurement
 unsigned int            loadCellForceCurrent              = 0;
+byte * forcePtr = (byte *) &loadCellForceCurrent;
 
 // Internal Objects
 bool configured = false;
@@ -73,11 +75,15 @@ void setup() {
   
   Serial.begin(115200);
 
-  while (!Serial) { ; } // wait for connection
+  while (Serial.available()) {
+    Serial.read();
+  }
+
+  // while (!Serial) { ; } // wait for connection
 
   // TODO: load all values from the PC before continuing
 
-  while (!configured) {
+  while (!configured || true) {
 
     if (Serial.available() >= INCOMING_PACKET_SIZE_BYTES) { parseIncomingSerial(); }
 
@@ -151,202 +157,203 @@ void parseIncomingSerial() {
 
   // remaining 5 bytes on buffer are the data bytes
   // what we do with them depends on the command
-  switch (commandByte) {
 
-    case 0:     // set shaft RPM max (16 bit) (unsigned int)
-      shaftRpmMaximum = serialDataToUnsignedInt();
+  if (commandByte == 0x00) {          // set shaft rpm maximum
+
+    shaftRpmMaximum = serialDataToUnsignedInt();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x01) {   // set shaft rpm maximum hysteresis
+
+    shaftRpmMaximumHyst = serialDataToUnsignedInt();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x02) {   // set shaft rpm rounding
+
+    shaftRpmRounding = serialDataToByte();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x03) {   // set shaft rpm timeout
+
+    shaftRpmTimeout = serialDataToUnsignedLong();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x04) {   // set shaft rpm target
+
+    shaftRpmDesired = serialDataToUnsignedInt();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x05) {   // set inlet kp
+
+    inletPidKp = serialDataToDouble();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x06) {   // set inlet ki
+
+    inletPidKi = serialDataToDouble();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x07) {   // set inlet kd
+
+    inletPidKd = serialDataToDouble();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x08) {   // set inlet min duty
+
+    byte tempInletMinDuty = serialDataToByte();
+
+    if (tempInletMinDuty > 100) {
+      sendTelemetry(false, true);
+    } else {
+      inletMinDuty = tempInletMinDuty;
       sendTelemetry(true, false);
-      break;
+    }
 
-    case 1:     // set shaft RPM max hysteresis (16 bit) (unsigned int)
-      shaftRpmMaximumHyst = serialDataToUnsignedInt();
+  } else if (commandByte == 0x09) {   // set inlet max duty
+
+    byte tempInletMaxDuty = serialDataToByte();
+
+    if (tempInletMaxDuty > 100) {
+      sendTelemetry(false, true);
+    } else {
+      inletMaxDuty = tempInletMaxDuty;
       sendTelemetry(true, false);
-      break;
+    }
 
-    case 2:      // set shaft RPM rounding (8 bit) (byte)
-      shaftRpmRounding = serialDataToByte();
+  } else if (commandByte == 0x0A) {   // enable/disable inlet override
+
+    inletOverrideActive = serialDataToBoolean();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x0B) {   // set inlet override duty cycle
+
+    if (inletOverrideActive) {
+      inletDutyDesired = serialDataToByte();
       sendTelemetry(true, false);
-      break;
+    } else {
+      sendTelemetry(false, true);
+    }
 
-    case 3:     // set shaft rpm timeout (32 bit) (unsigned long)
-      shaftRpmTimeout = serialDataToUnsignedLong();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x0C) {   // set outlet kp
+
+    outletPidKp = serialDataToDouble();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x0D) {   // set outlet ki
+
+    outletPidKi = serialDataToDouble();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x0E) {   // set outlet kd
+
+    outletPidKd = serialDataToDouble();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x0F) {   // set outlet min duty
+
+    byte tempOutletMinDuty = serialDataToByte();
+
+    if (tempOutletMinDuty > 100) {
+      sendTelemetry(false, true);
+    } else {
+      outletMinDuty = tempOutletMinDuty;
       sendTelemetry(true, false);
-      break;
+    }
 
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 4:     // set shaft rpm target (16 bit) (unsigned)
-      shaftRpmDesired = serialDataToUnsignedInt();
+  } else if (commandByte == 0x10) {   // set outlet max duty
+
+    byte tempOutletMaxDuty = serialDataToByte();
+
+    if (tempOutletMaxDuty > 100) {
+      sendTelemetry(false, true);
+    } else {
+      outletMaxDuty = tempOutletMaxDuty;
       sendTelemetry(true, false);
-      break;
+    }
 
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 5:     // set inlet kp (32 bit) (double)
-      inletPidKp = serialDataToDouble();
+  } else if (commandByte == 0x11) {   // set outlet target temperature
+
+    outletMaxTemperature = serialDataToByte();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x12) {   // enable/disable outlet override
+
+    outletOverrideActive = serialDataToBoolean();
+    sendTelemetry(true, false);
+
+  } else if (commandByte == 0x13) {   // set outlet override duty cycle
+
+    if (outletOverrideActive) {
+      outletDutyDesired = serialDataToByte();
       sendTelemetry(true, false);
-      break;
+    } else {
+      sendTelemetry(false, true);
+    }
 
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 6:     // set inlet ki (32 bit) (double)
-      inletPidKi = serialDataToDouble();
+  } else if (commandByte == 0x14) {   // set load cell resolution
+
+    byte tempResolution = serialDataToByte();
+
+    if (tempResolution == 64 || tempResolution == 128 ) {
+
+      loadCellResolution = tempResolution;
       sendTelemetry(true, false);
-      break;
 
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 7:     // set inlet kd (32 bit (double)
-      inletPidKd = serialDataToDouble();
-      sendTelemetry(true, false);
-      break;
+    } else {
+      sendTelemetry(false, true);
+    }
 
-    case 8:     // set inlet min duty (8 bit) (byte)
+  } else if (commandByte == 0x15) {   // set load cell factor
 
-      byte tempInletMinDuty = serialDataToByte();
+    loadCellFactor = serialDataToUnsignedInt();
+    sendTelemetry(true, false);
 
-      if (tempInletMinDuty > 100) {
-        sendTelemetry(false, true);
-      } else {
-        inletMinDuty = tempInletMinDuty;
-        sendTelemetry(true, false);
-      }
+  } else if (commandByte == 0x16) {   // telemetry request (no data)
 
-      break;
+    shredSerialData(5);
+    sendTelemetry(false, false);
 
-    case 9:     // set inlet max duty (8 bit) (byte)
+  } else if (commandByte == 0x17) {   // set configured bit
 
-      byte tempInletMaxDuty = serialDataToByte();
-
-      if (tempInletMaxDuty > 100) {
-        sendTelemetry(false, true);
-      } else {
-        inletMaxDuty = tempInletMaxDuty;
-        sendTelemetry(true, false);
-      }
-
-      break;
-
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 10:     // enable/disable inlet override (8 bit) (boolean)
-      inletOverrideActive = serialDataToBoolean();
-      sendTelemetry(true, false);
-      break;
-
-    case 11:    // set inlet override duty cycle (8 bit) (byte)
-      
-      if (inletOverrideActive) {
-        inletDutyDesired = serialDataToByte();
-        sendTelemetry(true, false);
-      } else {
-        sendTelemetry(false, true);
-      }
-
-      break;
-
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 12:    // set outlet kp (32 bit) (double)
-      outletPidKp = serialDataToDouble();
-      sendTelemetry(true, false);
-      break;
-
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 13:    // set outlet ki (32 bit) (double)
-      outletPidKi = serialDataToDouble();
-      sendTelemetry(true, false);
-      break;
-
-    // TODO: UPDATE PID TOO WHEN THIS HAPPENS
-    case 14:    // set outlet kd (32 bit) (double)
-      outletPidKd = serialDataToDouble();
-      sendTelemetry(true, false);
-      break;
-
-    case 15:    // set outlet min duty (8 bit) (byte)
-
-      byte tempOutletMinDuty = serialDataToByte();
-
-      if (tempOutletMinDuty > 100) {
-        sendTelemetry(false, true);
-      } else {
-        outletMinDuty = tempOutletMinDuty;
-        sendTelemetry(true, false);
-      }
-
-      break;
-
-    case 16:    // set outlet max duty (8 bit) (byte)
-
-      byte tempOutletMaxDuty = serialDataToByte();
-
-      if (tempOutletMaxDuty > 100) {
-        sendTelemetry(false, true);
-      } else {
-        outletMaxDuty = tempOutletMaxDuty;
-        sendTelemetry(true, false);
-      }
-
-      break;
-
-    case 17:    // set outlet target temperature (8 bit) (byte)
-
-      outletMaxTemperature = serialDataToByte();
-      sendTelemetry(true, false);
-      break;
-    
-    case 18:    // enable/disable outlet override (8 bit) (boolean)
-
-      outletOverrideActive = serialDataToBoolean();
-      sendTelemetry(true, false);
-      break;
-
-    case 19:    // set outlet override duty cycle (8 bit) (byte)
-
-      if (outletOverrideActive) {
-        outletDutyDesired = serialDataToByte();
-        sendTelemetry(true, false);
-      } else {
-        sendTelemetry(false, true);
-      }
-
-      break;
-
-    // TODO: UPDATE LOAD CELL WHEN THIS CHANGES
-    case 20:    // set load cell resolution (8 bit) (byte)
-
-      byte tempResolution = serialDataToByte();
-
-      if (tempResolution == 64 || tempResolution == 128 ) {
-
-        loadCellResolution = tempResolution;
-        sendTelemetry(true, false);
-
-      } else {
-        sendTelemetry(false, true);
-      }
-
-      break;
-
-    // TODO: UPDATE LOAD CELL WHEN THIS HAPPENS
-    case 21:    // set load cell factor (16 bit) (unsigned int)
-
-      loadCellFactor = serialDataToUnsignedInt();
-
-      sendTelemetry(true, false);
-      break;
-
-    case 22:  // telemetry request (no data)
-      shredSerialData(5);
-      sendTelemetry(false, false);
-      break;
-
-    case 23:  // set configured
-      shredSerialData(5);
-      configured = true;
-      sendTelemetry(true, false);
-      break;
+    shredSerialData(5);
+    configured = true;
+    sendTelemetry(true, false);
 
   }
 
 }
 
 void sendTelemetry(bool pass, bool fail) {
+
+  // status
+  // pass/fail
+  byte status = 0b00000000;
+  if (pass) {
+    bitSet(status, 5);
+  } 
+  
+  if (fail) {
+    bitSet(status, 6);
+  }
+  // add error telemetry here
+  Serial.write(status);
+
+  // shaft rpm
+  Serial.write(rpmPtr, 2);
+
+  // measured load cell force
+  Serial.write(forcePtr, 2);
+
+  // inlet duty cycle
+  Serial.write(inletDutyDesired);
+
+  // outlet duty cycle
+  Serial.write(outletDutyDesired);
+
+  // outlet water temperature
+  Serial.write(outletTemperatureCurrent);
 
 }
 
